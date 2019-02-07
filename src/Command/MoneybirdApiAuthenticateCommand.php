4e2a914d4e12709ccd7d3b27a750bb9530c1254f
Jan-Marten de Boer <github@johmanx.com>
2019-02-07 21:41:17 +0100

    Add Moneybird API commands
    Add commands to help authenticate and debug the API connection.

---
+++ src/Command/MoneybirdApiAuthenticateCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Command;
+
+use Picqer\Financials\Moneybird\Connection;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MoneybirdApiAuthenticateCommand extends Command
+{
+    /** @var string */
+    protected static $defaultName = 'moneybird:api:authenticate';
+
+    /** @var Connection */
+    private $connection;
+
+    /**
+     * Constructor.
+     *
+     * @param Connection $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ): int {
+        $table = new Table($output);
+
+        $table->addRow(
+            [
+                'Authorize URL',
+                $this->connection->getAuthUrl()
+            ]
+        );
+        $table->addRow(
+            [
+                'Token URL',
+                $this->connection->getTokenUrl()
+            ]
+        );
+        $table->addRow(
+            [
+                'Access token',
+                $this->connection->getAccessToken()
+            ]
+        );
+        $table->addRow(
+            [
+                'Administration ID',
+                $this->connection->getAdministrationId()
+            ]
+        );
+
+        $table->render();
+
+        return 0;
+    }
+}
---
+++ src/Command/MoneybirdApiEntityCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Command;
+
+use Picqer\Financials\Moneybird\Model;
+use Picqer\Financials\Moneybird\Moneybird;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MoneybirdApiEntityCommand extends Command
+{
+    /** @var string */
+    protected static $defaultName = 'moneybird:api:entity';
+
+    /** @var Moneybird */
+    private $client;
+
+    /**
+     * Constructor.
+     *
+     * @param Moneybird $client
+     */
+    public function __construct(Moneybird $client)
+    {
+        parent::__construct();
+        $this->client = $client;
+    }
+
+    /**
+     * Configure the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setDescription('Fetch API entities');
+        $this->addArgument(
+            'entity',
+            InputArgument::REQUIRED,
+            'The entity type to query.'
+        );
+        $this->addArgument(
+            'method',
+            InputArgument::OPTIONAL,
+            'The API method to invoke.',
+            'getAll'
+        );
+        $this->addArgument(
+            'arguments',
+            InputArgument::IS_ARRAY,
+            'Arguments to supply to the API call',
+            []
+        );
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ): int {
+        $resource = call_user_func(
+            [
+                $this->client,
+                $input->getArgument('entity')
+            ]
+        );
+
+        $result = (array)call_user_func_array(
+            [
+                $resource,
+                $input->getArgument('method')
+            ],
+            $input->getArgument('arguments')
+        );
+
+        /** @var Model $model */
+        foreach ($result as $model) {
+            $entity = json_decode($model->json());
+
+            $output->writeln(
+                json_encode(
+                    $entity,
+                    JSON_PRETTY_PRINT
+                    | JSON_UNESCAPED_SLASHES
+                    | JSON_UNESCAPED_UNICODE
+                    | JSON_NUMERIC_CHECK
+                )
+            );
+        }
+
+        return 0;
+    }
+}
